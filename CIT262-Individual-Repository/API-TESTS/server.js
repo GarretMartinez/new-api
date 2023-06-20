6f294a0c613f7222a10c21517e395618fd5199b1
Garret Martinez <gtmrz22@gmail.com>
2023-06-20 09:44:28 -0600

    Added server.js and dockerfile to api-tests

---
+++ CIT262-Individual-Repository/API-TESTS/server.js
@@ -0,0 +1,14 @@
+const express = require('express0');
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to my Sean's backend application");
+});
+
+application.get("/sean",(req,res)=>{
+    res.send("Hello Sean!");
+});
+
+app.listen(3000,()=>{
+    console.log("Listening");
+});
